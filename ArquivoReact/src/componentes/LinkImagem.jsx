0f775959cedad8cc441f6ad8b736835ecfbf7ec0
brunopdrocha <brunopdrocha@gmail.com>
2023-10-11 17:15:33 -0300

    Primeiro arquivo React

---
+++ ArquivoReact/src/componentes/LinkImagem.jsx
@@ -0,0 +1,7 @@
+const LinkImagem = () => (
+    <a href="https://s2-techtudo.glbimg.com/JsE244mucjKWLYtNgeiDyfVYlJQ=/0x129:1024x952/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/7/i/ME2AxRRoygUyFPCDe0jQ/3.png">
+      <img src="https://s2-techtudo.glbimg.com/JsE244mucjKWLYtNgeiDyfVYlJQ=/0x129:1024x952/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/7/i/ME2AxRRoygUyFPCDe0jQ/3.png" alt="Descrição da Imagem" />
+    </a>
+  );
+  
+  export default LinkImagem;
---
+++ ArquivoReact/src/pages/Segundo.jsx
@@ -0,0 +1,11 @@
+import LinkPrimeiro from "../componentes/LinkPrimeiro";
+import LinkImagem  from "../componentes/LinkImagem";
+const Segundo = () => {
+ return (<div>
+            <h5>Segundo</h5>
+            <LinkPrimeiro />
+            <LinkImagem/>
+        </div>);
+};
+
+export default Segundo;
